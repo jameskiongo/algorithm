1abfdafa5ec599b061d5a51f8697e2e519274287
jameskiongo <kiongojames2000@gmail.com>
2025-07-03 12:10:55 +0300

    cs50 filter less assignment finished sepia filter

--- memory/assignment/filter-less/helpers.c
+++ memory/assignment/filter-less/helpers.c
@@ -3,8 +3,8 @@
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
-  for (int h = 0; h <= height; h++) {
-    for (int w = 0; w <= width; w++) {
+  for (int h = 0; h < height; h++) {
+    for (int w = 0; w < width; w++) {
       float sum = 0.0;
       sum = sum + image[h][w].rgbtBlue;
       sum = sum + image[h][w].rgbtGreen;
@@ -19,7 +19,35 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
 }
 
 // Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width]) { return; }
+void sepia(int height, int width, RGBTRIPLE image[height][width]) {
+  for (int h = 0; h < height; h++) {
+    for (int w = 0; w < width; w++) {
+      int original_blue = image[h][w].rgbtBlue;
+      int original_green = image[h][w].rgbtGreen;
+      int original_red = image[h][w].rgbtRed;
+      // sepia
+      int sepia_blue = round((.272 * original_red) + (.534 * original_green) +
+                             (.131 * original_blue));
+      if (sepia_blue > 255) {
+        sepia_blue = 255;
+      }
+      int sepia_green = round((.349 * original_red) + (.686 * original_green) +
+                              (.168 * original_blue));
+      if (sepia_green > 255) {
+        sepia_green = 255;
+      }
+      int sepia_red = round((.393 * original_red) + (.769 * original_green) +
+                            (.189 * original_blue));
+      if (sepia_red > 255) {
+        sepia_red = 255;
+      }
+      image[h][w].rgbtBlue = sepia_blue;
+      image[h][w].rgbtGreen = sepia_green;
+      image[h][w].rgbtRed = sepia_red;
+    }
+  }
+  return;
+}
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width]) { return; }
